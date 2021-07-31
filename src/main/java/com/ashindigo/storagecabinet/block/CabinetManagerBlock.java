3b690b16ab12085500730f0aa524211b3f6a9ae1
Ash <5542898+AshIndigo@users.noreply.github.com>
2021-07-31 23:45:36 +0700

    Minor fixes

--- src/main/java/com/ashindigo/storagecabinet/block/CabinetManagerBlock.java
+++ src/main/java/com/ashindigo/storagecabinet/block/CabinetManagerBlock.java
@@ -11,6 +11,7 @@ import net.minecraft.world.MenuProvider;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
